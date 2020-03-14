50578165788ff6f9576c5e7c3088e3065bd01bf9
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-03-14 16:39:51 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -33,10 +33,10 @@ if (!session_id())
 
 // Call Facebook API
 
-$facebook = new \Facebook\Facebook([
+$facebook = new Facebook\Facebook([
   'app_id'      => '626566744568306',
   'app_secret'     => '033f61fe00877f39eed2b85ebce12fd2',
-  'default_graph_version'  => 'v6.0'
+  'default_graph_version'  => 'v6.0',
 ]);
 
 
